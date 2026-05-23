6b4d0d96751c545e77b015b9c5b8f803dd695fe5
ArtLighter <shuklin1995@list.ru>
2026-05-23 20:10:23 +0700

    Небольшие исправления;

--- src/main/java/com/artlighter/glucosecontrolservice/diary/entity/enumeration/InsulinType.java
+++ src/main/java/com/artlighter/glucosecontrolservice/diary/entity/enumeration/InsulinType.java
@@ -3,5 +3,6 @@ package com.artlighter.glucosecontrolservice.diary.entity.enumeration;
 public enum InsulinType {
     LONG,
     SHORT_CARBS,
-    SHORT_CORRECTION
+    SHORT_CORRECTION,
+    SHORT
 }
