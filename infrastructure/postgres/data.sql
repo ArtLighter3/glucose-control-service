f8406c7319d72ef837b2b1af157beaa8e662ca52
ArtLighter <shuklin1995@list.ru>
2026-06-21 23:18:27 +0700

    Исправил имя перезапускаемой секвенции

--- infrastructure/postgres/data.sql
+++ infrastructure/postgres/data.sql
@@ -1,4 +1,4 @@
-ALTER SEQUENCE serviceuser_id_seq RESTART;
+ALTER SEQUENCE service_user_id_seq RESTART;
 INSERT INTO service_user(username, password, first_name, middle_name, last_name, email, birth_date)
     VALUES ('root','$2a$12$HB8VG0RtaTXHpaB.0fyzC./Lw20b7BkZF.a1F1mRz68zI1Xpx0h6a',
             'root', null, 'root', null, null);
